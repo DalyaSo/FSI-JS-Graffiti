00c91a56ba8f27aaa40afcc9f5330daecb9b7da2
Dalya Sohl <dalyasohl@hotmail.com>
2022-01-26 21:56:16 -0500

    text align left and font color

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+    
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+for (let x = 0; x < dogName.length; x++) {
+    dogName[x].style.textAlign = 'left'
+    
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = "#0000FF"
+//dogName.style.textAlign = "left"
 // Select some elements...
